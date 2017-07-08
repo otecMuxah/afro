95f4f6bc05fc0ffc1c5953e1c7203e3ff24098d7
Misha <suxx_2000@hotmail.com>
2017-07-09 01:22:59 +0300

    index page

--- js/main.js
+++ js/main.js
@@ -1,8 +1,34 @@
 $(window).load(function() {
     $('.carousel').slick({
-        infinite: true,
+        infinite: false,
         slidesToShow: 5,
-        slidesToScroll: 5
+        slidesToScroll: 5,
+        responsive: [
+            {
+                breakpoint: 2370,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 1500,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 1300,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+
     });
     $('.form__select').selectmenu();
 });
